bbe49f3e66fea07da8f2c30e91029c8a42fb25ca
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2018-12-03 23:11:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,15 @@ Client.on("guildMemberRemove", member => {
  
 
   Client.on('guildMemberAdd', member => {
-  member.send(`
+  member.send(`**نضام أنفايت جديد  :herb: 
+  
+  :bow_and_arrow:  2 Invites ==> 10k Credits  :bow_and_arrow: 
+  
+  :bow_and_arrow:  4 invites ==> Visa 25$  :bow_and_arrow:
+  
+  :bow_and_arrow:  ملاحضة : فيزا صالحة لشراء نيترو :Nitro: و تفعيل بايبال :Paypal:  و خوادم  :bow_and_arrow:**
+  
+  https://discord.gg/XENJNEQ
 [${member}]**`);
 
 
