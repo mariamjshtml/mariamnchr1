888395986ae5c6598c4be5fb8c3c56f140cf96e2
mariamjshtml <45521454+mariamjshtml@users.noreply.github.com>
2018-12-20 21:18:17 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ console.log("Welcome Again  by Fox!");
 
 
 Client.on("guildMemberRemove", member => {
-  member.send(`**سلام عليكم شباب:wave: 
+  member.send(`**
+   سلام عليكم شباب:wave: 
 انتمى انكم تدخلوا سيرفر ده:100: 
 :يقدم سيرفر:stuck_out_tongue: 
 قيف اوايات على فورتنايت سبوتفاي تاقات كلشي:scream: 
@@ -18,6 +19,23 @@ Client.on("guildMemberRemove", member => {
 https://discord.gg/hJEnCE
 
 [${member}]**`);
+
+  });
+ 
+
+  Client.on('guildMemberAdd', member => {
+  member.send(`**
+ سلام عليكم شباب:wave: 
+انتمى انكم تدخلوا سيرفر ده:100: 
+:يقدم سيرفر:stuck_out_tongue: 
+قيف اوايات على فورتنايت سبوتفاي تاقات كلشي:scream: 
+انفايت ريويرد جيب 3 تأخذ 15 الف كريدت:hushed: 
+فعاليات:ok_hand: 
+بوتات ديسكورد:kiss: 
+سولف الخ....:ribbon:
+https://discord.gg/hJEnCE
+[${member}]**`);
+
 
   });
 
